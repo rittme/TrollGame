f7fbdc92fd12cee14294088d9679f909cc9b5dfc
Bernardo Rittmeyer <bernardo@rittme.com>
2015-03-21 18:00:25 +0100

    removed big troll

---
+++ app/src/main/java/fr/insa_lyon/bg4if/trollgame/MyApp.java
@@ -0,0 +1,11 @@
+package fr.insa_lyon.bg4if.trollgame;
+
+import android.app.Application;
+
+/**
+ * Created by vcaen on 23/03/2015.
+ */
+public class MyApp extends Application{
+    public static final string PREF_FOLDER_IMAGE =  "pref_folder_image";
+
+}
